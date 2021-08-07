396b037c404ab3f0c579df29b8e50f7858a3c5d6
Lateef <lateefogunbadejo@yahoo.com>
2021-08-07 21:33:49 +0100

    remove hardcoded question

--- src/pages/Question/Question.tsx
+++ src/pages/Question/Question.tsx
@@ -65,9 +65,7 @@ const Question = () => {
 
             <Progress currentQuestion={currentQuestion + 1} total={amount} />
 
-            {/* <p className="question-text" dangerouslySetInnerHTML={{__html: questions[currentQuestion]?.question}} /> */}
-
-            <p className="question-text">This is the correct spelling of "Supercalifragilisticexpialidocious"</p>
+            <p className="question-text" dangerouslySetInnerHTML={{__html: questions[currentQuestion]?.question}} />
 
             <PurpleButton label="true" onClick={() => selectAnswer("True")} />
             <OutlineButton
